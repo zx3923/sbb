0301cfc4388919945cd3e6fce9bb74c639582a5e
zx3923 <seop3923@gmail.com>
2022-09-20 14:19:49 +0900

    test

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        System.out.println("sbb");
+        return "sbb";
+    }
+
+}
